717e2bb68012db81bb24dfa38fef00a2f9eb97dc
jesusbeb <jesusbelb@gmail.com>
2024-06-26 14:54:52 -0600

    Creamos el controller de Topic con el metodo para registrar nuevo Topico

---
+++ src/main/java/com/alura/ForoHub_Challenge/controller/TopicosController.java
@@ -0,0 +1,27 @@
+package com.alura.ForoHub_Challenge.controller;
+
+import com.alura.ForoHub_Challenge.dto.DatosRegistroTopico;
+import com.alura.ForoHub_Challenge.model.Topico;
+import com.alura.ForoHub_Challenge.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+
+//RestController para indicar a Spring que es un controller
+//RequestMapping para mapear la ruta o path topicos
+@RestController
+@RequestMapping("/topicos")
+public class TopicosController {
+
+    @Autowired
+    private TopicoRepository topicoRepository;
+
+    //Metodo para registrar topico. Request de tipo POST
+    @PostMapping
+    public void RegistrarTopico(@RequestBody @Valid DatosRegistroTopico datosRegistroTopico){
+        topicoRepository.save(new Topico(datosRegistroTopico));
+    }
+}
