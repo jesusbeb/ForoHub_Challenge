e459e9abb9b2b1d1d90e74e6304d43429973f948
jesusbeb <jesusbelb@gmail.com>
2024-06-26 15:01:16 -0600

    Creando el TopicoRepository para las consultas en la BD

---
+++ src/main/java/com/alura/ForoHub_Challenge/repository/TopicoRepository.java
@@ -0,0 +1,10 @@
+package com.alura.ForoHub_Challenge.repository;
+
+import com.alura.ForoHub_Challenge.model.Topico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//Repository para hacer todo el proceso de gestion con la BD a nivel del CRUD
+//JPARepository necesita dos parametros genericos, el primero es el tipo de objeto que se va a
+//guardar (Entidad Medico). El segundo es el tipo de objeto del id (Long)
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+}
